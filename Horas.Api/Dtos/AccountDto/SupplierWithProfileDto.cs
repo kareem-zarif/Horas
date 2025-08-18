011b3c10e8507330c9a2f3b4875e7c3a136dd842
MahmoudAbdulatif <mahmoudabdullitif@gmail.com>
2025-08-18 10:00:35 +0300

    last

---
+++ Horas.Api/Dtos/AccountDto/SupplierWithProfileDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Horas.Api.Dtos.AccountDto
+{
+    public class SupplierWithProfileDto
+    {
+        public Guid Id { get; set; } // Supplier Id
+        public string? SupplierName { get; set; }
+
+        // بيانات من الـ Person
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        // بيانات من الـ SellerProfile
+        public string? StoreName { get; set; }
+        public string? BusinessType { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public string? WebsiteUrl { get; set; }
+        public string? StoreLogoUrl { get; set; }
+    }
+}
